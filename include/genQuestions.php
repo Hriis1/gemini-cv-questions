b4dd6ea44cccd5dd415c1d5534a4f5b99ad80bf8
hrishd <hrishd@abv.bg>
2024-11-19 11:24:21 +0200

    env

--- include/genQuestions.php
+++ include/genQuestions.php
@@ -1,5 +1,9 @@
 <?php
+require_once '../vendor/autoload.php';
 require_once "generateQuestionsGemini.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['job_title'])) {
         $geminiAPIkey = $_ENV['GEMINI_FLASH_API_KEY'];
